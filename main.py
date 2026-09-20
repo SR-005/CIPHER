4d461b992536e60b91db93f25a2e6f1fdfdfa9f6
Sreeram V Gopal <sreeramvg100@gmail.com>
2026-09-20 13:18:34 +0530

    deque implementation I

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2
 import asyncio
 import threading
 import time
+import datetime
+from collections import deque
 from fastapi.responses import FileResponse
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
@@ -18,6 +20,13 @@ app.add_middleware(
 
 cameraurl="http://127.0.0.1:5000/"      #url of the live stream. code from camsimulation.py
 currentframebytes=None
+
+preeventframes=90
+posteventframes=90
+framebuffer=deque(maxlen=preeventframes)
+isrecording=False
+posteventcounter=0
+videowriter=None
 
 model=YOLO("yolov8n.pt")    #initializing model
 
@@ -37,6 +46,37 @@ def capturestreamthread():
             result=model(frame,classes=[0], verbose=False)      #classes=[0] targets persons only, verbose=False suppresses thinking logs
             labelledframe=result[0].plot()
 
+            framebuffer.append(labelledframe)           #continuosly push the frame into the frame buffer
+
+            boxes=result[0].boxes                   #check if there is any event happening
+
+            #if any event is happening and if its not already recording then, save pre event clips
+            if len(boxes)>0  and not isrecording:
+                print("Violence Detected!!")
+                isrecording=True
+                posteventcounter=0
+
+                timestamp=datetime.datetime.now().strftime("%Y%m%d_%H%M%S")         #set timestamp in the video recording
+                filename=f"incident_{timestamp}.mp4"
+                fourcc=cv2.VideoWriter_fourcc(*'mpv4')          #fourcc is an indentifier which tells the system which compression algorithm to apply
+                height,width,_=labelledframe.shape              #set video height and width same as labelled video height and width
+                videowriter=cv2.VideoWriter(filename,fourcc,30.0,(width,height))
+
+                #dump already recorded frames into filename as pre-event footage
+                for bufferedframe in framebuffer:
+                    videowriter.write(bufferedframe)
+                print("Pre Event Frames are saved!")
+
+            #for post event clip saving
+            if isrecording:
+                videowriter.write(labelledframe)
+                posteventcounter+=1
+
+                if posteventcounter>=posteventframes:   #when the post event queue is full, stop the recording
+                    videowriter.release()
+                    isrecording=False
+                    framebuffer.clear()
+                    print("Post Event Recording Compelete")
 
             #compressing video frames to JPEG Images
             _,buffer=cv2.imencode('.jpg',labelledframe)
